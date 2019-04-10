2f2d3a019183abc6eb35100c751f2b96c1cdabe4
JustAccount1702 <37345260+JustAccount1702@users.noreply.github.com>
2019-04-10 07:18:58 +0200

    Trying out with vectors

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "matrix.h"
-#include "Fractions.h"
+#include "matrix.hpp"
+#include "Fraction.hpp"
 
 int main()
 {
@@ -9,7 +9,12 @@ int main()
 	std::cout << "Transfer matrix" << std::endl << mat << std::endl;
 	std::cout << "Transfer matrix ^2" << std::endl << (mat^2) << std::endl;
 	std::cout << "Transfer matrix ^3" << std::endl << (mat^3) << std::endl;
-
-	system("pause");
+	Matrix<Fraction> baseVector(1,4);
+	for (int i = 0; i < 4; ++i)
+		baseVector.setValue(0,i, Fraction(1,4));
+	
+	for (int i = 0; i < 4; ++i)
+		baseVector = baseVector * mat;
+	std::cout << baseVector << std::endl;
 	return 0;	
 }
