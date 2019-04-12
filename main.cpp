f135064540b97274d50b813469820a4ccc79f441
JustAccount1702 <mkgolitsino@mail.ru>
2019-04-12 22:16:34 +0300

    Little localization

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ int main()
 {
 	Matrix<Fraction> mat("input.txt");
 
-	std::cout << "Transfer matrix" << std::endl << mat << std::endl;
+	std::cout << "Инфинитезальная матрица: " << std::endl << mat << std::endl;
     Matrix<Fraction> baseVector(1,4);
 	for (int i = 0; i < 4; ++i)
 		baseVector.setValue(0, i, Fraction(1,4));
     Matrix<Fraction> result = baseVector;
 	for (int i = 0; i < 40; ++i)
 		result = baseVector * mat;
-    std::cout << result << std::endl;
+    std::cout << "Вектор распределения вероятностей 40 степени: " << std::endl << result << std::endl;
     std::cout << result.convert(double()) << std::endl;
 	return 0;
 }
